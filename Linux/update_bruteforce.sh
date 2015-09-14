c27620d584a7e10d7967fcea59049889ffd42c0c
Ralph deGennaro <rdegennaro@sscps.org>
2015-09-14 17:56:44 -0400

    Update update_bruteforce.sh

--- Linux/update_bruteforce.sh
+++ Linux/update_bruteforce.sh
@@ -2,7 +2,7 @@
 # quick link to master copy = http://goo.gl/V8GjhV
 apt-get update
 # doing both because Kernels not done in former, and system utils not done in later
-apt-get upgrade
+apt-get -y upgrade
 aptitude -y --full-resolver safe-upgrade
 apt-get -y autoremove
 apt-get clean
