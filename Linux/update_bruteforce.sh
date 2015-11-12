5097b66c94e57ea1be2e8636821322847bb5072c
Ralph deGennaro <rmdegennaro@gmail.com>
2015-11-12 17:51:56 -0500

    add commenting and stuff
    so easier to see when output moves from one command to next

--- Linux/update_bruteforce.sh
+++ Linux/update_bruteforce.sh
@@ -1,8 +1,20 @@
 # do updates without "looking" & cleanup afterwards
 # quick link to master copy = https://goo.gl/SViJLz
+echo ===============================
+echo   Retreiving updated packages
+echo ===============================
 apt-get update
 # doing both because Kernels not done in former, and system utils not done in later
+echo ===============================
+echo   Installing updated packages
+echo ===============================
 apt-get -y upgrade
+echo ===============================
+echo   Forcing held-back packages
+echo ===============================
 aptitude -y --full-resolver safe-upgrade
+echo ===============================
+echo   Cleaning up packages
+echo ===============================
 apt-get -y autoremove
 apt-get clean
