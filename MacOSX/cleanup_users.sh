d8ffb54f2b526a9cf9a087e1cc96ac9581a1e49c
Joe McSheffrey <jmcsheffrey@sscps.org>
2016-01-12 14:22:31 -0500

    Update cleanup_users.sh

--- MacOSX/cleanup_users.sh
+++ MacOSX/cleanup_users.sh
@@ -1,21 +1,23 @@
-
-
 # This script will delete all local user home folders in /Users
 # except sscpslocal, student, teacher, Shared
-# that are 14 days or older, until 10GB space is free
+# that are 14 days or older or until 10GB space is free
+
+# how much space is free?
+freeDiskSpace=df -k | grep -E '^/dev/disk1' | awk '{print $4}'
+
+# How much space do we want free (in kilobytes)?
+targetFreeDiskSpace=10000000
+
+# we want to exit immediately if space is fine
+if [ freeDiskSpace > targetFreeDiskSpace ]; then
+   exit
 
 # create array of local users to keep
 declare -a exceptions=(sscpslocal student teacher Shared)
 
-# How much space to we want free (in kilobytes)?
-targetFreeDiskSpace=10000000
-
 # use find to single out folders older than 14 days
 # and populate them to array folders
 folders=(`find /Users -maxdepth 1 -mindepth 1 -type d -ctime +14`)
-
-# how much space is free?
-freeDiskSpace=df -k | grep -E '^/dev/disk1' | awk '{print $4}'
 
 # loop through array and delete folders not in exceptions array
 # until 10GB disk space is free, then exit
