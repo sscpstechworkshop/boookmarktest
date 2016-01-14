62673c12903dfc8d63892429dd6b35150d64c30b
Ralph deGennaro <rdegennaro@sscps.org>
2016-01-14 11:26:35 -0500

    added in comments for what script should do

--- Linux/hist_rolloff_zoneminder.sh
+++ Linux/hist_rolloff_zoneminder.sh
@@ -1,1 +1,5 @@
-# just placeholder
+#!/bin/bash
+#
+# simple script to 1) determine size of new files to copy (rsync?) from location a to location b
+#                  2) remove files oldest files if location b needs more space
+#                  3) actually copy (rsync?) the files from location a to location b
