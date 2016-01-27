33e562425f5a43d9b1d779071b7acb43224b06a3
Joe McSheffrey <jmcsheffrey@sscps.org>
2016-01-27 10:04:31 -0500

    Update cleanup_users.sh

--- MacOSX/cleanup_users.sh
+++ MacOSX/cleanup_users.sh
@@ -6,14 +6,14 @@
 # how much space is free?
 # NOTE:  be mindful of external USB/drives as they will be mounted and confuse df command here
 freeDiskSpace=(`df -k | grep -E '^/' | awk '{print $4}'`)
-echo "Initial free disk space is " $freeDiskSpace
+#echo "Initial free disk space is " $freeDiskSpace
 
 # How much space do we want free (in kilobytes)?
 targetFreeDiskSpace=10000000
 
 # exit immediately if free space is okay
 if [ $freeDiskSpace -gt $targetFreeDiskSpace ]; then
-   echo "Free space is fine, exiting script..."
+#   echo "Free space is fine, exiting script..."
    exit 0
 fi
 
@@ -34,7 +34,7 @@ for f in ${folders[@]}
 do
    groups=$(id -Gn $f)
    if [[ $groups =~ "SG_Policy_MacWriteLocal" ]]; then
-      echo "Adding "$f" to exception list because they are in local Mac group..."
+#      echo "Adding "$f" to exception list because they are in local Mac group..."
       exceptions+=($f)
    fi
 done
@@ -42,7 +42,7 @@ done
 # remove exceptions from folders array
 for e in ${exceptions[@]}
 do
-   echo "Removing from folders array because it is an exception: "$e
+#   echo "Removing from folders array because it is an exception: "$e
    # TODO: our test user folders that end _student are getting "student" removed because of static exception
    # I can live with that for now, need to get this done because drives are filling up
    folders=(${folders[@]/$e})
@@ -58,13 +58,13 @@ done
 # target disk space is free, then exit
 for f in ${folders[@]}
 do
-    echo "Deleting user folder:  "$f
+#    echo "Deleting user folder:  "$f
     rm -rf /Users/$f
     freeDiskSpace=(`df -k | grep -E '^/' | awk '{print $4}'`)
-    echo "After deleting "$f" there is "$freeDiskSpace" space left"
+#    echo "After deleting "$f" there is "$freeDiskSpace" space left"
     # check free space, if over target exit
     if [ $freeDiskSpace -gt $targetFreeDiskSpace ]; then
-       echo "Exiting script because free space is fine after deleting "$f
+#       echo "Exiting script because free space is fine after deleting "$f
        exit 0
     fi
 done
