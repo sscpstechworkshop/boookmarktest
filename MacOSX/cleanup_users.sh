d2045498970b57df38f7431220c6989b7769ffe5
Joe McSheffrey <jmcsheffrey@sscps.org>
2016-01-27 08:47:51 -0500

    Update cleanup_users.sh

--- MacOSX/cleanup_users.sh
+++ MacOSX/cleanup_users.sh
@@ -1,49 +1,40 @@
-# SCRIPT NOT COMPLETED 
+# SCRIPT NOT COMPLETED
 # This script will delete all local user home folders in /Users
 # except sscpslocal, student, teacher, Shared
 # and any AD users in SG_Policy_MacWriteLocal group
 # until 10GB space is free
 
 # how much space is free?
-freeDiskSpace=(`df -k | grep -E '^/dev/disk1' | awk '{print $4}'`)
+freeDiskSpace=(`df -k | grep -E '^/' | awk '{print $4}'`)
+echo "Initial free disk space is " $freeDiskSpace
 
 # How much space do we want free (in kilobytes)?
-targetFreeDiskSpace=10000000
+targetFreeDiskSpace=15000000
 
 # exit immediately if free space is okay
 if [ $freeDiskSpace -gt $targetFreeDiskSpace ]; then
-   exit
+   echo "Free space is fine, exiting script..."
+   exit 0
 fi
 
-# Populate the folders array with all folders in /Users 
+# Populate the folders array with all folders in /Users
 # sorted by oldest first
-folders=(`ls -rt`)
+folders=(`ls -rt /Users`)
+
+# troubleshooting contents of folders array
+for f in ${folders[@]}
+do
+   echo "In folders array: " $f
+done
 
 # create exceptions array
-exceptions=(sscpslocal student teacher Shared)
-for f in "${folders[@]}";
+exceptions=( sscpslocal student teacher Shared )
+for f in ${folders[@]}
 do
    groups=$(id -Gn $f)
    if [[ $groups =~ "SG_Policy_MacWriteLocal" ]]; then
-   	exceptions+=($f)
-   fi
-done
-
-# remove exceptions from folders array
-for e in "${exceptions[@]}";
-do
-   folders=(${folders[@]/$e})
-done
-
-# loop through folders array and delete until
-# target disk space is free, then exit
-while [ $f in folders ] 
-do
-      rm -rf /Users/$f
-      freeDiskSpace=(`df -k | grep -E '^/dev/disk1' | awk '{print $4}'`)
-   # check free space, if over target exit
-   if [ $freeDiskSpace -gt $targetFreeDiskSpace ]; then
-      exit
+      echo "Adding "$f" to exception list because they are in local Mac group..."
+      exceptions+=($f)
    fi
 done
 
