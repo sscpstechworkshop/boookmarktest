71fdb5236082ae14762494e02d4b014bc0a422e2
Ralph deGennaro <rdegennaro@sscps.org>
2016-03-14 14:06:39 -0400

    Update setup_new_manjaro-kde.sh

--- Linux/setup_new_manjaro-kde.sh
+++ Linux/setup_new_manjaro-kde.sh
@@ -1,6 +1,7 @@
 # keyring issue on installs from 12.15 download
 sudo pacman-key --init 
 sudo pacman-key --populate archlinux manjaro 
+sudo dirmngr < /dev/null
 sudo pacman-key --refresh-keys
 # clean out bad downloads/updates/etc.
 sudo pacman -Sc
