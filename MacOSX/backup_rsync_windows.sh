f78d438b4de31f987b555d7c3d8726257d241d55
Ralph deGennaro <rdegennaro@sscps.org>
2016-03-30 12:24:15 -0400

    Update backup_rsync_windows.sh

--- MacOSX/backup_rsync_windows.sh
+++ MacOSX/backup_rsync_windows.sh
@@ -1,1 +1,14 @@
 #! /bin/bash
+#####################################################################################
+#
+# Script Name:    backup_rsync_windows.sh
+# Script Usage:   This script will walk through an array of Windows Servers to backup
+#
+#                 You will probably want to do the following:
+#                   1.  change is the scriptname,
+#                   2.  adjust adjust list of server FQDNs & shares,
+#                   3.  adjust other paths if you don't like them.
+#
+# Script Updates: 201603301224 - Ralph deGennaro - First added boilerplate stuff.
+#
+#####################################################################################
