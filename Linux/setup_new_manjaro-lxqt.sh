29de662db23862f657925073555fc85f4c718f93
Ralph deGennaro <rmdegennaro@gmail.com>
2016-04-18 17:23:29 -0400

    Update setup_new_manjaro-lxqt.sh

--- Linux/setup_new_manjaro-lxqt.sh
+++ Linux/setup_new_manjaro-lxqt.sh
@@ -2,15 +2,17 @@
 # update whole system
 sudo pacman -Syyuu
 # install packages as needed
-sudo pacman -R chromium
 sudo pacman -S base-devel binutils git
+sudo pacman -S libreoffice
 sudo pacman -S wine
 sudo pacman -S gimp inkscape audacity blender
 sudo pacman -S filezilla firefox
 sudo pacman -S xscreensaver menda-themes menda-themes-dark qtcurve-qt4 qtcurve-qt5 xcursor-menda menda-circle-icon-theme faenza-icon-theme openbox-themes xcursor-menda
 sudo pacman -S kate konsole dolphin
-sudo pacman -S libreoffice
-# these can't be run as root
+# setup Google Chrome
+sudo pacman -R chromium
 yaourt -S google-chrome
+sudo pacman -S kdebase-kdialog ttf-liberation
+# setup Atom text/development editor
 yaourt -S atom-editor
 #adjust sudoers
