36d6a8ddbd19aa2d8aa39efaf3bfd25459141bc6
Joe McSheffrey <jmcsheffrey@sscps.org>
2016-05-03 12:46:30 -0400

    Update init_home_folders.sh

--- MacOSX/init_home_folders.sh
+++ MacOSX/init_home_folders.sh
@@ -1,14 +1,14 @@
 # This script will query an AD OU and create user folders based on the return
 
 OU="OU=Student,OU=Users,OU=Test,OU=SSCPS,DC=ad,DC=sscps,DC=org"
-folders=( Documents Downloads Movies Music Desktop Public )
+folders=( "Documents" "Downloads" "Movies" "Pictures" "Music" "Desktop" "Public" )
 local_storage_path="/Storage"
 local_server="TEST-MAC-FS"
 
 # Populate users array with objects in specified OU, using test/test123 AD account
 users=(`ldapsearch -H ldap://ad.sscps.org -x -D "test@ad.sscps.org" -w test123 -b $OU -s sub "(objectClass=user)" sAMAccountName | awk '/^sAMAccountName: /{print $NF}'`)
 
-for u in ${users[@]}
+for u in "${users[@]}"
 do
    home_dir=(`ldapsearch -H ldap://ad.sscps.org -x -D "test@ad.sscps.org" -w test123 -b $OU -s sub "(samaccountname=$u)" homeDirectory | awk '/^homeDirectory: /{print $NF}'`)
 # Create different variables for server, share and user root folder
@@ -29,11 +29,11 @@ do
    mkdir -p $new_home_dir
    chown $u $new_home_dir 
    chmod u+rwX $new_home_dir
-      for f in ${folders[@]}
-      do
-         mkdir -p $new_home_dir/${folders[@]}
-         chown $u $new_home_dir/${folders[@]}
-         chmod u+rwX $new_home_dir/${folders[@]}
-      done
+   for f in "${folders[@]}"
+   do
+      echo "Folder variable = " ${folders[@]}
+      mkdir -p $new_home_dir/${folders[@]}
+      chown $u $new_home_dir/${folders[@]}
+      chmod u+rwX $new_home_dir/${folders[@]}
+   done
 done
-
