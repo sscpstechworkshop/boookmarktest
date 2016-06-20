443fbe8647eb6209b9073a53e2a436a7fcc8d885
Joe McSheffrey <jmcsheffrey@sscps.org>
2016-06-20 14:57:07 -0400

    Update rename_workstation.sh

--- MacOSX/rename_workstation.sh
+++ MacOSX/rename_workstation.sh
@@ -1,6 +1,12 @@
+########################################################################################
 # This script renames the three computer name variables to WKN<MAC ADDRESS>
+# If an argument is supplied that will be used instead
 # It should be run right after deploying an image to the machine
-# Script location should be /usr/local/bin
+# Script location is /usr/local/bin
+#
+# NOTE: This script will also disable the cleanup_users.plist if an argument is supplied
+#       on the assumption that the machine is for Staff
+########################################################################################
 
 name=(`ifconfig en0 | awk '/ether/{print $2}' | sed -e 's/://g'`)
 scutil --set ComputerName WKN$name
