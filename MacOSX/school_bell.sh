032fb6fa389595ac6e8e1234487a1353d6a641d8
Joe McSheffrey <jmcsheffrey@sscps.org>
2016-08-20 12:22:14 -0400

    Update school_bell.sh

--- MacOSX/school_bell.sh
+++ MacOSX/school_bell.sh
@@ -15,69 +15,96 @@ day=`date "+%A"`        # Friday
 hour=`date "+%H"`       # 15 (24hr format)
 minute=`date "+%M"`     # 45 
 currentDate=$month/$dayOfMonth/$year
+currentTime=$hour:$minute
 
-# Check if /Users/Shared/BellSchedule/bellschedule_settings.conf exists
 if [ ! -f /Users/Shared/BellSchedule/bellschedule_settings.conf ]; then
-    echo "bellschedule_settings.conf file not found!   Exiting." | logger -s >> /Users/Shared/BellSchedule/logs/bellschedule.log
-    exit 1;
+    mkdir -p /Users/Shared/BellSchedule/
+    echo $currentDate>bellschedule_settings.conf
+    curl -o /Users/Shared/BellSchedule/bellschedule_$day.conf 'http://files.sscps.org/<location>/bellschedule_$day'
 else
     storedDate=`head -1 /Users/Shared/BellSchedule/bellschedule_settings.conf`
     if [ "$currentDate" = "$storedDate" ]; then
         echo "bellschedule_settings.conf file has today's date.   Exiting." | logger -s >> /Users/Shared/BellSchedule/logs/bellschedule.log
         exit 0;
+    else
+        curl -o /Users/Shared/BellSchedule/bellschedule_$day.conf 'http://files.sscps.org/<location>/bellschedule_$day'
     fi
 fi
 
-# conf file exists and the date is not today, so we need to grab the schedule file
-curl -o /Users/Shared/BellSchedule/bellschedule_$day.conf 'http://files.sscps.org/<location>/bellschedule_$day'
+# Give the possible download a moment to finish
+sleep 5
+
 scheduleFile=/Users/Shared/BellSchedule/bellschedule_$day.conf
 
 # populate scheduleArray from scheduleFile
-# NOTE: do we need a sleep 5 here to assure that the download has completed?
 if [ ! -f /Users/Shared/BellSchedule/bellschedule_$day.conf ]; then
     echo "bellschedule_$day.conf doesn't exist!   Exiting." | logger -s >> /Users/Shared/BellSchedule/logs/bellschedule.log
     exit 1;
 else
+    IFS=','
     scheduleArray=(`cat $scheduleFile`)
 fi
 
-# build individual schedule arrays (scheduleFile should have exactly 4 lines?)
-# first line in scheduleFile will be default schedule
-IFS=',' read -ra defaultSchedule <<< "${scheduleArray[0]}"
+for i in ${scheduleArray[@]}; do
+    if [ "$i" = "default" ]; then
+        bellSchedule=${scheduleArray[$i]}
+    fi
+    if [ "$i" = "$currentDate" ]; then
+        bellSchedule=${scheduleArray[$i]}
+    fi
+done
 
-# second line in scheduleFile is first exceptions
-IFS=',' read -ra exceptionSchedule1 <<< "${scheduleArray[1]}"
+# remove date element (should be first) from schedule array
+unset bellSchedule[0]
 
-# third line in scheduleFile is second exceptions
-IFS=',' read -ra exceptionSchedule2 <<< "${scheduleArray[2]}"
-
-# fourth line in scheduleFile is third exceptions
-IFS=',' read -ra exceptionSchedule3 <<< "${scheduleArray[3]}"
-
-# Match current date with exception dates to see if we are not on default schedule
-if [ "${exceptionsSchedule1[0]}" = "$currentDate" ]; then
-    activeSchedule=("${exceptionSchedule1[@]}")
-else if [ "${exceptionsSchedule2[0]}" = "$currentDate" ]; then
-    activeSchedule=("${exceptionSchedule2[@]}")
-else if [ "${exceptionsSchedule3[0]}" = "$currentDate" ]; then
-    activeSchedule=("${exceptionSchedule3[@]}")
-else
-    activeSchedule=("${defaultSchedule[@]}")
-    
-# if activeSchedule array only has 1 element (e.g. it's Christmas) exit script
-if [ ${#activeSchedule[@]} -eq 1 ]; then
-    echo "activeSchedule has no times.  Exiting." | logger -s >> /Users/Shared/BellSchedule/logs/bellschedule.log
-    exit 0;
+# if bellSchedule has no times, exit
+if [ ${#bellSchedule[@]} -eq 0 ]; then
+     echo "Schedule has no times.  Exiting." | logger -s >> /Users/Shared/BellSchedule/logs/bellschedule.log
+     exit 0;
 fi
 
-# make sure system volume isn't muted, this puts volume at about 50 percent
-osascript -e "set volume 4"
-
-for time in ${activeSchedule[@]}; do
-    if [ "$time" = "${activeSchedule[@]}" ]; then    # TODO check for future times and exit if found
+for time in ${bellSchedule[@]}; do
+    if [ "$time" = "$currentTime" ]; then
+        # set volume to 50%
+        osascript -e "set volume 4"
         afplay /Users/Shared/BellSchedule/school_bell.mp3
         exit 0;
+    fi
 done
+
+
+
+# build individual schedule arrays (scheduleFile should have exactly 4 lines?)
+# first line in scheduleFile will be default schedule
+# IFS=',' read -ra defaultSchedule <<< "${scheduleArray[0]}"
+
+# second line in scheduleFile is first exceptions
+# IFS=',' read -ra exceptionSchedule1 <<< "${scheduleArray[1]}"
+
+# third line in scheduleFile is second exceptions
+# IFS=',' read -ra exceptionSchedule2 <<< "${scheduleArray[2]}"
+
+# fourth line in scheduleFile is third exceptions
+# IFS=',' read -ra exceptionSchedule3 <<< "${scheduleArray[3]}"
+
+# Match current date with exception dates to see if we are not on default schedule
+# if [ "${exceptionsSchedule1[0]}" = "$currentDate" ]; then
+#    activeSchedule=("${exceptionSchedule1[@]}")
+# else if [ "${exceptionsSchedule2[0]}" = "$currentDate" ]; then
+#    activeSchedule=("${exceptionSchedule2[@]}")
+# else if [ "${exceptionsSchedule3[0]}" = "$currentDate" ]; then
+#     activeSchedule=("${exceptionSchedule3[@]}")
+# else
+#     activeSchedule=("${defaultSchedule[@]}")
+    
+# if activeSchedule array only has 1 element (e.g. it's Christmas) exit script
+# if [ ${#activeSchedule[@]} -eq 1 ]; then
+#     echo "activeSchedule has no times.  Exiting." | logger -s >> /Users/Shared/BellSchedule/logs/bellschedule.log
+#     exit 0;
+# fi
+
+
+
     
 
 
