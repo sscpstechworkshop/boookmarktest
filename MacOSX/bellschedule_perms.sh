b683dac6c1ceb22759a37679b5444b36db4eeeee
Joe McSheffrey <jmcsheffrey@sscps.org>
2016-08-22 16:37:14 -0400

    Create bellschedule_perms.sh

---
+++ MacOSX/bellschedule_perms.sh
@@ -0,0 +1,6 @@
+# Resetting perms for /Users/Shared/BellSchedule
+
+chown -R root:staff /Users/Shared/BellSchedule
+chmod -R u+rwx /Users/Shared/BellSchedule
+chmod -R g+rwx /Users/Shared/BellSchedule
+chmod -R o+rx /Users/Shared/BellSchedule
