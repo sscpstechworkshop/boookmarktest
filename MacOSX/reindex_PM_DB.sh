d1760fb12c0290a2072bdd8f595436cf1ee58ada
Joe McSheffrey <jmcsheffrey@sscps.org>
2016-10-17 14:56:02 -0400

    Update reindex_PM_DB.sh

--- MacOSX/reindex_PM_DB.sh
+++ MacOSX/reindex_PM_DB.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 # This script reindexes the profile manager database in OS X Server
 
+# Moving this to launchd where it will run on a schedule as root
 # test if run as sudo/root
-if [ $(whoami) != "root" ]; then
-        echo "Please run as sudo/root"
-        exit 1
-fi
+#if [ $(whoami) != "root" ]; then
+#        echo "Please run as sudo/root"
+#        exit 1
+#fi
 
 # declare some variables
 PGUSER=_devicemgr
 PMSOCKET="/Library/Server/ProfileManager/Config/var/PostgreSQL/"
 PSQL=/Applications/Server.app/Contents/ServerRoot/usr/bin/psql
-TABLEFILE=PMTables.txt
+TABLEFILE=/tmp/PMTables.txt
 
 # stopping profile manager
-echo "Stopping Profile Manager..."
+#echo "Stopping Profile Manager..."
 serveradmin stop devicemgr
 
 # starting postgres
-echo "Starting PostgreSQL..."
+#echo "Starting PostgreSQL..."
 serveradmin start postgres
 
 # dig the different tables from the PSQL database and store it to TABLEFILE
@@ -29,9 +30,10 @@ sed -i "" -e '1,2d' -i "" -e '$!{N;N;H;}' $TABLEFILE
 
 #the Table names should be of format name_of_the_table, thus readable simply with read
 while read line; do
-	echo "Reindexing table public.$line..."
+#	echo "Reindexing table public.$line..."
 	$PSQL -U $PGUSER -h $PMSOCKET -d devicemgr_v2m0 -c "REINDEX table public.$line;"
 done <$TABLEFILE
-echo "Reindexing complete, starting Profile Manager..."
+
+#echo "Reindexing complete, starting Profile Manager..."
 serveradmin start devicemgr
 exit 0
