cbf9e40450d46efafb98977648c392ae75aecde8
Joe McSheffrey <jmcsheffrey@sscps.org>
2017-05-15 16:06:58 -0400

    Update config_wkn.sh

--- MacOSX/config_wkn.sh
+++ MacOSX/config_wkn.sh
@@ -2,11 +2,11 @@
 # If an argument is supplied (faculty machine):
 #    - rename workstation to argument supplied
 #    - disable cleanup_users.plist
-#    - enable school_bell.plist
+#    - enable bellschedule.plist
 # If no argument (student machine):
 #    - rename workstation to WKN<MAC Address>
 #    - enable cleanup_users.plist
-#    - disable school_bell.plist
+#    - disable bellschedule.plist
 #
 # script should be installed at /usr/local/bin
 ########################################################################################
@@ -16,14 +16,14 @@ if [ $# -eq 0 ]; then
    name=WKN$mac_address
    # ENABLE cleanup_users.plist
    sed -i "" 's/false/true/g' /Library/LaunchDaemons/cleanup_users.plist
-   # DISABLE school_bell.plist
-   sed -i "" 's/true/false/g' /Library/LaunchAgents/school_bell.plist
+   # DISABLE bellschedule.plist
+   sed -i "" 's/true/false/g' /Library/LaunchAgents/bellschedule.plist
 else
    name=$1
    # DISABLE cleanup_users.plist
    sed -i "" 's/true/false/g' /Library/LaunchDaemons/cleanup_users.plist
-   # ENABLE school_bell.plist
-   sed -i "" 's/false/true/g' /Library/LaunchAgents/school_bell.plist
+   # ENABLE bellschedule.plist
+   sed -i "" 's/false/true/g' /Library/LaunchAgents/bellschedule.plist
 fi
 
 scutil --set ComputerName $name
