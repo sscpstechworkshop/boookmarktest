c3ea69015353dbb98b6856d6153e85894393d7a5
Joe McSheffrey <jmcsheffrey@sscps.org>
2017-08-02 15:34:00 -0400

    Update reset_chrome.sh

--- MacOSX/reset_chrome.sh
+++ MacOSX/reset_chrome.sh
@@ -2,3 +2,7 @@
 
 rm -r /Users/student/Library/Application\ Support\Google\Chrome
 rm -r /Users/teacher/Library/Application\ Support\Google\Chrome
+
+# The following command will remove a local account from MacOS
+# dscl localhost delete /Local/Default/Users/sscpswlan
+# rm -rf /Users/sscpswlan
