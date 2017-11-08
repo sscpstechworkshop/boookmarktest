2f0931def3690522653fafc59ae1d1edf1f84f3b
Ralph deGennaro <rdegennaro@sscps.org>
2017-11-08 11:12:57 -0500

    get MAC addresses for untangle rules

--- SQL/glpi.sql
+++ SQL/glpi.sql
@@ -1,4 +1,25 @@
-
+-- get MAC addresses for untangle rules
+CREATE VIEW list_macaddress_untangle AS
+select 'n/a' as Population
+    , cmp.name as Name
+    , os.name as 'Device Type'
+    , netcard.mac as 'MAC Address'
+    , '' as 'Static IP'
+    , '' as 'Passed IP or MAC?'
+    , (case
+        when cmp.operatingsystems_id = 3 then 'Chromebook_Login'
+        else 'GW_Stu_Lower'
+        end) as 'Destination Rack'
+  from glpi_computers as cmp
+  join glpi_networkports as netcard
+    on cmp.id = netcard.items_id
+  join glpi_operatingsystems as os
+    on cmp.operatingsystems_id = os.id
+  join glpi_locations as loc
+    on cmp.locations_id = loc.id
+  where cmp.operatingsystems_id in (3,6)
+    and loc.completename not like '%ROLL-OFF%'
+  order by os.name, cmp.name
 
 
 -- roll off summary
@@ -20,8 +41,8 @@ select `ips`.`name` AS `ip_address`
     , `cmp`.`name` AS `comp_name`
     , `cmp`.`contact_num` AS `fqdn`
     , `cmp`.`comment` AS `comment_comp`
-  from (((`glpiprod`.`glpi_ipaddresses` `ips` 
-          join `glpiprod`.`glpi_computers` `cmp` on((`ips`.`mainitems_id` = `cmp`.`id`))) 
-          join `glpiprod`.`glpi_networknames` `netnames` on((`ips`.`items_id` = `netnames`.`id`))) 
-          join `glpiprod`.`glpi_networkports` `netports` on((`netnames`.`items_id` = `netports`.`id`))) 
+  from (((`glpiprod`.`glpi_ipaddresses` `ips`
+          join `glpiprod`.`glpi_computers` `cmp` on((`ips`.`mainitems_id` = `cmp`.`id`)))
+          join `glpiprod`.`glpi_networknames` `netnames` on((`ips`.`items_id` = `netnames`.`id`)))
+          join `glpiprod`.`glpi_networkports` `netports` on((`netnames`.`items_id` = `netports`.`id`)))
   order by `ips`.`name`,`netports`.`mac`,`cmp`.`name`;
