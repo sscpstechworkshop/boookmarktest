4b7c77b3997eeed71be1817275b05895d2e07f5e
Joe McSheffrey <jmcsheffrey@sscps.org>
2018-01-26 09:38:55 -0500

    added level3/4 options to bell schedule

--- MacOSX/bellschedule_test.sh
+++ MacOSX/bellschedule_test.sh
@@ -43,7 +43,7 @@ if [ ! -f $confFile ]; then
    exit
 else
    remoteDir=`head -1 $confFile`
-   if [ "${remoteDir}" != "middleschool" ] && [ "${remoteDir}" != "highschool" ]; then
+   if [ "${remoteDir}" != "highschool" ] && [ "${remoteDir}" != "level3" ] && [ "${remoteDir}" != "level4" ]; then
       sendToLog "remoteDir has an invalid value of $remoteDir.  Aborting!"
       exit
    fi
