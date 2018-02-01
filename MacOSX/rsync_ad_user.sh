54124fbffa3c7b94be8388a3c08453a05c8cc757
Ralph deGennaro <rdegennaro@sscps.org>
2018-02-01 11:04:11 -0500

    change mount location

--- MacOSX/rsync_ad_user.sh
+++ MacOSX/rsync_ad_user.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 # Bash skips errors and resumes by default
 
+# set tempfolder for mounting
+vTempMountPoint="~/TempMnt/"
+
 # manually set home folder
 #vHomeFolder="\\<server>\FacStaffUserFiles\<username>\"
 vHomeFolder=""
-mount -t smbfs $vHomeFolder/Documents ~/Documents
-mount -t smbfs $vHomeFolder/Movies ~/Movies
-mount -t smbfs $vHomeFolder/Music ~/Music
-mount -t smbfs $vHomeFolder/Pictures ~/Pictures
-mount -t smbfs $vHomeFolder/Downloads ~/Downloads
-mount -t smbfs $vHomeFolder/Desktop ~/Desktop
-mount -t smbfs $vHomeFolder/Public ~/Public
+mount -t smbfs $vHomeFolder/Documents "$vTempMountPoint"Documents
+mount -t smbfs $vHomeFolder/Movies "$vTempMountPoint"Movies
+mount -t smbfs $vHomeFolder/Music "$vTempMountPoint"Music
+mount -t smbfs $vHomeFolder/Pictures "$vTempMountPoint"Pictures
+mount -t smbfs $vHomeFolder/Downloads "$vTempMountPoint"Downloads
+mount -t smbfs $vHomeFolder/Desktop "$vTempMountPoint"Desktop
+mount -t smbfs $vHomeFolder/Public "$vTempMountPoint"Public
