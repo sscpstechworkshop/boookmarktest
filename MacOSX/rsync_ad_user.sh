b7667cc6c883932f06da8a277414380efbbda750
Ralph deGennaro <rdegennaro@sscps.org>
2018-02-01 12:10:15 -0500

    Update rsync_ad_user.sh

--- MacOSX/rsync_ad_user.sh
+++ MacOSX/rsync_ad_user.sh
@@ -4,10 +4,12 @@
 # set tempfolder for mounting
 vTempMountPoint=~/TempMnt/
 
-# manually set home folder
+# set everything up, don't forget to manually enter home folder location
 #vHomeFolder="\\\\<server>\\FacStaffUserFiles\\<username>\\"
 vHomeFolder=""
 mkdir -p "$vTempMountPoint"Documents
+
+# mount all the folders
 mount -t smbfs $vHomeFolder/Documents "$vTempMountPoint"Documents
 mkdir -p "$vTempMountPoint"Movies
 mount -t smbfs $vHomeFolder/Movies "$vTempMountPoint"Movies
@@ -21,3 +23,14 @@ mkdir -p "$vTempMountPoint"Desktop
 mount -t smbfs $vHomeFolder/Desktop "$vTempMountPoint"Desktop
 mkdir -p "$vTempMountPoint"Public
 mount -t smbfs $vHomeFolder/Public "$vTempMountPoint"Public
+
+#do rsync stuff
+
+#unmount folders
+umount "$vTempMountPoint"Documents
+umount "$vTempMountPoint"Movies
+umount "$vTempMountPoint"Music
+umount "$vTempMountPoint"Pictures
+umount "$vTempMountPoint"Downloads
+umount "$vTempMountPoint"Desktop
+umount "$vTempMountPoint"Public
