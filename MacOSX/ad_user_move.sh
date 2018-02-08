062ceaf7c7c248c830c9774e0d9e2834a76ee28e
Ralph deGennaro <rdegennaro@sscps.org>
2018-02-08 16:26:58 -0500

    Update ad_user_move.sh

--- MacOSX/ad_user_move.sh
+++ MacOSX/ad_user_move.sh
@@ -6,10 +6,46 @@
 # set folder that is storing user files 
 vTempSource=/Users/Shared/<username>/
 
-rsync -amrtv --delete-before --progress "$vTempSource"Documents ~/
-rsync -amrtv --delete-before --progress "$vTempSource"Movies ~/
-rsync -amrtv --delete-before --progress "$vTempSource"Music ~/
-rsync -amrtv --delete-before --progress "$vTempSource"Pictures ~/
-rsync -amrtv --delete-before --progress "$vTempSource"Downloads ~/
-rsync -amrtv --delete-before --progress "$vTempSource"Desktop ~/
-rsync -amrtv --delete-before --progress "$vTempSource"Public ~/
+#rsync -amrtv --delete-before --progress "$vTempSource"Documents ~/
+#rsync -amrtv --delete-before --progress "$vTempSource"Movies ~/
+#rsync -amrtv --delete-before --progress "$vTempSource"Music ~/
+#rsync -amrtv --delete-before --progress "$vTempSource"Pictures ~/
+#rsync -amrtv --delete-before --progress "$vTempSource"Downloads ~/
+#rsync -amrtv --delete-before --progress "$vTempSource"Desktop ~/
+#rsync -amrtv --delete-before --progress "$vTempSource"Public ~/
+
+# Move files
+mv "$vTempSource"Documents/* ~/Documents
+mv "$vTempSource"Movies/* ~/Movies
+mv "$vTempSource"Music/* ~/Music
+mv "$vTempSource"Pictures/* ~/Pictures
+mv "$vTempSource"Downloads/* ~/Downloads
+mv "$vTempSource"Desktop/* ~/Desktop
+mv "$vTempSource"Public/* ~/Public
+
+# Change owner from sscpslocal to user
+chown -Rv <username> ~/Documents
+chown -Rv <username> ~/Movies
+chown -Rv <username> ~/Music
+chown -Rv <username> ~/Pictures
+chown -Rv <username> ~/Downloads
+chown -Rv <username> ~/Desktop
+chown -Rv <username> ~/Public
+
+# change permission to new user
+chmod -Rv u=rwX ~/Documents
+chmod -Rv u=rwX ~/Movies
+chmod -Rv u=rwX ~/Music
+chmod -Rv u=rwX ~/Pictures
+chmod -Rv u=rwX ~/Downloads
+chmod -Rv u=rwX ~/Desktop
+chmod -Rv u=rwX ~/Public
+
+# remove permissions from everyone else
+chmod -Rv go-rwX ~/Documents
+chmod -Rv go-rwX ~/Movies
+chmod -Rv go-rwX ~/Music
+chmod -Rv go-rwX ~/Pictures
+chmod -Rv go-rwX ~/Downloads
+chmod -Rv go-rwX ~/Desktop
+chmod -Rv go-rwX ~/Public
